38bbc19eb047e84345e9d4e042ef9b5972a8cf1a
Sebastian Kippe <sebastian@kip.pe>
2013-04-18 22:05:16 +0200

    Clean up tasks module

--- tasks.js
+++ tasks.js
@@ -1,76 +1,106 @@
-remoteStorage.defineModule('tasks', function(privateClient) {
-  function getUuid() {
-    var i, random, uuid = '';
-    for (i=0; i<32; i++) {
-      random = Math.random()*16 | 0;
-      if(i === 8 || i === 12 || i === 16 || i === 20) {
-        uuid += '-';
-      }
-      uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random) ).toString(16);
-    }
-    return uuid;
-  }
-  function Todo(title) {
-    this.id = getUuid();
-    this.title = title;
-    this.completed = false;
-  }
+//
+// Tasks
+//
+// TODO Add some documentation
+//
 
-  return {
-    exports: {
-      getTodos: function() {
-        console.log('in getTodos, calling getAll');
-        return privateClient.getAll('todos/');
+remoteStorage.defineModule('tasks', function(privateClient) {
+
+  var util = remoteStorage.util;
+
+  privateClient.declareType('todo', {
+    "type": "object",
+    "description": "A single-line task on your todo list.",
+    "properties": {
+      "id": {
+        "type": "string",
+        "format": "id"
       },
-      addTodo: function(text) {
-        var todo = new Todo(text);
-        privateClient.storeObject('todo-list-item', 'todos/'+todo.id, todo);
+      "title": {
+        "type": "string"
       },
-      setTodo: function(id, todo) {
-        console.log('setTodo', id, todo);
-        privateClient.storeObject('todo-list-item', 'todos/'+id, todo);
-      },
-      setTodoText: function(id, text) {
-        privateClient.getObject('todos/'+id).then(function(obj) {
-          console.log('updating text of item '+id+' from "'+obj.text+'" to "'+text+'"');
-          obj.title = text;
-          privateClient.storeObject('todo-list-item', 'todos/'+id, obj);
+      "completed": {
+        "type": "boolean",
+        "default": false
+      }
+    },
+    "required": [ "id", "title", "completed" ]
+  });
+
+  var tasks = {
+
+    todos: {
+
+      getAll: function() {
+        return privateClient.getAll('todos/', 'todo').then(function(todos) {
+          return Object.keys(todos).map(function(id) {
+            return todos[id];
+          });
         });
       },
+
+      store: function(object) {
+        var todo = privateClient.buildObject('todo', object);
+
+        return privateClient.storeObject('todo', 'todos/'+todo.id, todo).
+          then(function(){
+            return todo;
+          });
+      },
+
+      setTodoTitle: function(id, title) {
+        return privateClient.getObject('todos/'+id).then(function(todo) {
+          todo.title = title;
+          return privateClient.storeObject('todo', 'todos/'+id, todo);
+        });
+      },
+
       setTodoCompleted: function(id, value) {
-        privateClient.getObject('todos/'+id).then(function(obj) {
-          obj.completed = value;
-          privateClient.storeObject('todo-list-item', 'todos/'+id, obj);
+        return privateClient.getObject('todos/'+id).then(function(todo) {
+          todo.completed = value;
+          return privateClient.storeObject('todo', 'todos/'+id, todo);
         });
       },
-      setAllTodosCompleted: function(value) {
-        privateClient.getAll('todos/').then(function(objs) {
-          for(var i in objs) {
-            if(objs[i].completed != value) {
-              objs[i].completed = value;
-              privateClient.storeObject('todo-list-item', 'todos/'+i, objs[i]);
-            }
-          }
+
+      setAllCompleted: function(value) {
+        var self = this;
+        return self.getAll().then(function(todos) {
+          return util.asyncEach(todos, function(todo) {
+            return self.store(util.extend(todo, { completed: true }));
+          });
         });
       },
-      removeTodo: function(id) {
-        privateClient.remove('todos/'+id);
+
+      remove: function(id) {
+        return privateClient.remove('todos/'+id);
       },
-      removeAllCompletedTodos: function() {
-        privateClient.getAll('todos/').then(function(objs) {
-          for(var i in objs) {
-            if(objs[i].completed) {
-              privateClient.remove('todos/'+i);
-            }
-          }
-        });
-      },
-      onChange: function( cb ) {
-        privateClient.on('change', function(event) {
-          console.log('change', event);
-          cb(event.oldValue, event.newValue);
+
+      removeAllCompleted: function() {
+        var self = this;
+        return self.getAll().then(function(todos) {
+          return util.asyncEach(todos, function(todo) {
+            return self.remove(todo.id);
+          });
         });
       }
     }
   };
+
+  util.extend(tasks.todos, util.getEventEmitter('change'));
+
+  // You can use the change event like so from your app:
+  //
+  //     remoteStorage.tasks.todos.on('change', function(event){
+  //       console.log(event.origin, event.id, event.oldValue, event.newValue);
+  //     })
+  //
+  privateClient.on('change', function(event) {
+    var md = event.relativePath.match(/^todos\/(.+)$/);
+    if (md) {
+      event.id = md[1];
+      tasks.todos.emit('change', event);
+    }
+  });
+
+  return { exports: tasks };
 });
